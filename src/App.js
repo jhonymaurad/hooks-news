0a826d912fd1d859bd7fc57f38aeaf91d8fcd2b3
jhony maurad <jhonymaurad@gmail.com>
2020-02-17 22:13:54 -0500

    Added useEffect, useState, ad useRef hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    getResults();
+  }, []);
+
+  const getResults = async () => {
+    const response = await Axios.get(
+      `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+
+    setResults(response.data.hits);
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+          ref={searchInputRef}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href="{result.url}">{result.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
